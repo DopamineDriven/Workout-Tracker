033134761c8d1d1dee9689bed7955d26e5485682
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-17 03:22:22 -0600

    mongoose schema made

---
+++ models/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const workoutSchema = new Schema({
+    //using seed.js from seeders, define day and exercises
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Enter exercise type"
+            },
+            name: {
+                type: String,
+                trim: true,
+                required: "Enter exercise name"
+            },
+            duration: {
+                type: Number,
+                required: "Enter exercise duration in minutes"
+            },
+            weight: {
+                type: Number,
+                //weight in pounds
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            distance: {
+                type: Number
+                //distance in miles
+            }
+        }
+    
+    ]
+})
