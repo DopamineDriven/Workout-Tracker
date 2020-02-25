283776ffb759792780f5a002d77c4787fc5ef7aa
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-25 12:15:14 -0600

    troubleshooting put

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get('/stats', (request, response) => {
 
 
 // getting Workout Schema from models/workout.js
-app.get('/api/workouts', (request, response) => {
+app.get('/api/workouts', async (request, response) => {
     db.Workout.find({})
         .then(dbWorkout => {
             response.json(dbWorkout)
@@ -54,15 +54,54 @@ app.get('/api/workouts', (request, response) => {
 });
 
 // create new workout-->post
-app.post('/api/workouts', (request, response) => {
+app.post('/api/workouts', async (request, response) => {
     const workout = new WorkoutModel({ exercises: request.body });
     WorkoutModel.create(workout)
         .then(dbWorkout => {
             response.json(dbWorkout)
         })
         .catch(error => {
-            response.send(error)
+            response
+                .status(500)
+                .send(error)
         })
+});
+
+// edit existing workout-->put
+app.put('api/workouts/:id', (request, response) => {
+    WorkoutModel.findOneAndUpdate(
+        { _id: request.params.id }, 
+        { exercises: request.body }, 
+        { new: true }
+        )
+            .then((dbWorkout) => {
+                if(!dbWorkout) {
+                    console.log(error)
+                    response
+                        .status(404)
+                        .send(error)
+                } else {
+                    response.json(dbWorkout)
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+                response
+                    .status(500)
+                    .send("error occurred")
+            })
+});
+
+app.put('')
+
+app.get('/api/workouts/range', async (request, response) => {
+    db.Workout.find({})
+    .then(dbWorkout => {
+        response.json(dbWorkout)
+    })
+    .catch(error => {
+        response.send(error)
+    })
 });
 
 
