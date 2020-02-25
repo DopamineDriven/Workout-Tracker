651ec277391a3b5a6fe2aa1cf6172c228ff1664c
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-25 09:57:57 -0600

    html routes established

--- server.js
+++ server.js
@@ -5,10 +5,36 @@ const Workout = require('./models/workout.js');
 const mongoose = require('mongoose');
 const db = require('./models');
 
-mongoose.connect('mongodb://localhost/Workout-Tracker', 
-{
+mongoose.connect('mongodb://localhost/Workout-Tracker', {    
     useNewUrlParser: true, 
-    useFindAndModify: false, 
+    useFindAndModify: false,
+    useCreateIndex: true,
     useUnifiedTopology: true
 });
 
+const PORT = process.env.PORT || 3333;
+
+
+// https://www.npmjs.com/package/morgan
+// using predefined format string'tiny'
+app.use(morgan('tiny'));
+
+// serving static content for app from public __dir
+app.use(express.static('public'));
+
+// parse requests as JSON 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// html routes
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname+'/public/index.html'))
+});
+
+app.get('/exercise', (request, response) => {
+    response.sendFile(path.join(__dirname+'/public/exercise.html'))
+});
+
+app.get('/stats', (request, response) => {
+    response.sendFile(path.join(__dirname+'/public/stats.html'))
+});
