add74768c0563bb1f6084ac0f4191ac1af102a9e
Andrew Ross <andrew.simpson.ross@gmail.com>
2020-02-29 14:25:41 -0600

    seeders

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(`mongodb://user1234:Wordpass311@ds053784.mlab.com:53784/heroku_mffnqcmb`, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
